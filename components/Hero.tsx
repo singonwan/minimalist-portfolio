0e4a36db6facd2930f538fccf6396558ca05e6f2
George's Macbook <georgewan@georgess-mbp.home>
2024-07-20 20:27:31 -0400

    initial commit with half the hero done

---
+++ components/Hero.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-left-10 -top-40 h-screen md:-left-32 md:-top-20"
+          fill="white"
+        />
+        <Spotlight
+          className="top10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className="absolute left-0 top-0 flex h-screen w-full items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black-100 dark:bg-grid-white/[0.03]">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+      </div>
+      <div className="relative z-10 my-20 flex justify-center">
+        <div className="max-w-[89vw flex flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
+          <h2 className="max-w-80 text-center text-xs uppercase tracking-widest text-blue-100">
+            Dynamic Web Magic with Next.js
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
