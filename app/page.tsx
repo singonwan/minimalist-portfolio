f5d13dffc4b70ce0334691c5be7d26eecb73c908
George's Macbook <georgewan@georgess-mbp.home>
2024-07-25 00:59:43 -0400

    adding floating nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import Hero from '@/components/Hero';
-import Image from 'next/image';
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
-	return (
-		<main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-			<div className="max-w-7xl w-full">
-				<Hero />
-			</div>
-		</main>
-	);
+  return (
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+      <div className="w-full max-w-7xl">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
+        <Hero />
+      </div>
+    </main>
+  );
 }
