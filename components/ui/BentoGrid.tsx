44e96652d3b5638d7dcb42808add207b2b5c5b32
singonwan <singonwan@gmail.com>
2024-07-25 23:14:39 -0400

    adding tech stack background image, need to adjust size, make it smaller later

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -46,7 +46,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "group/bento relative row-span-1 flex flex-col justify-between space-y-4 overflow-hidden rounded-3xl border border-transparent bg-white p-4 shadow-input transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+        "group/bento relative row-span-1 flex flex-col justify-between space-y-4 overflow-hidden rounded-3xl border border-white/[0.1] shadow-input transition duration-200 hover:shadow-xl dark:shadow-none",
         className,
       )}
       style={{
@@ -55,7 +55,7 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className={`${id === 6} && flex h-full justify-center`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="absolute h-full w-full">
           {img && (
             <img
@@ -66,19 +66,19 @@ export const BentoGridItem = ({
           )}
         </div>
         <div
-          className={`absolut -bottom-5 right-0 ${id === 5 && "w-full opacity-80"}`}
+          className={`absolute -bottom-5 right-0 ${id === 5 && "w-full opacity-80"}`}
         >
           {spareImg && (
             <img
               src={spareImg}
               alt={spareImg}
-              className="object-cover, h-full w-full object-center"
+              className="h-full w-full object-cover object-center"
             />
           )}
         </div>
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 flex items-center justify-center font-bold text-white" />
+            <div className="pointer-events-none absolute inset-0 z-50 flex items-center justify-center px-4 text-center text-3xl font-bold text-white md:text-4xl lg:text-7xl" />
           </BackgroundGradientAnimation>
         )}
 
@@ -96,8 +96,36 @@ export const BentoGridItem = ({
           >
             {title}
           </div>
+
+          {id === 2 && <GridGlobe />}
+
+          {id === 3 && (
+            <div className="absolute -right-3 flex w-fit gap-1 lg:-right-2 lg:gap-5">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {["React.js", "Next.js", "TypeScript"].map((item) => (
+                  <span
+                    key={item}
+                    className="rounded-lg bg-[#10132E] px-3 py-2 text-center text-xs opacity-50 lg:px-3 lg:py-4 lg:text-base lg:opacity-100"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="rounded-lg bg-[#10132E] px-3 py-4 text-center" />
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="rounded-lg bg-[#10132E] px-3 py-4 text-center" />
+                {["HTML", "Python", "MongoDB"].map((item) => (
+                  <span
+                    key={item}
+                    className="rounded-lg bg-[#10132E] px-3 py-2 text-center text-xs opacity-50 lg:px-3 lg:py-4 lg:text-base lg:opacity-100"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
-        {id === 2 && <GridGlobe />}
       </div>
     </div>
   );
