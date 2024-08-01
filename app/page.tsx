680e25cbfa551e7711a7680da9e0daf8597d9af3
singonwan <singonwan@gmail.com>
2024-08-01 14:48:52 -0400

    removed login from navbar, and added functionality and proper styling to it

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,14 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
