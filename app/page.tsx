12e067886b4637d2553c562fb18e57de3cbc34a9
singonwan <singonwan@gmail.com>
2024-08-03 20:38:55 -0400

    adding footer with email button and social links

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -15,6 +16,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Experience />
+        <Footer />
       </div>
     </main>
   );
